c6ec4a47fe8b529aaca0ef9ca84ce4eb9be1d169
Mehdi8932 <googigle@gmail.com>
2024-12-06 11:40:00 +0200

    Update passwall2x.sh

--- passwall2x.sh
+++ passwall2x.sh
@@ -38,7 +38,7 @@ if [ "$SNNAP" == "SNAPSHOT" ]; then
 
 echo -e "${YELLOW} SNAPSHOT Version Detected ! ${NC}"
 
-rm -f passwalls.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
+rm -f passwalls.sh && wget https://raw.githubusercontent.com/Mehdi8932/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
 
 exit 1
 
